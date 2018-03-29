6c0b41b806c9a5382e11374755726746af09993a
tqshao <tq_shao@yahoo.com>
2018-03-28 21:34:29 -0400

    Added gitignore.

--- ros/build/traj_follower/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;message_runtime;ros_chrono_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lros_chrono".split(';') if "-lros_chrono" != "" else []
-PROJECT_NAME = "traj_follower"
-PROJECT_SPACE_DIR = "/home/tq/Documents/Git/traj_follower/ros/devel"
-PROJECT_VERSION = "0.0.0"
